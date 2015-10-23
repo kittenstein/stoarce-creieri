3954bd00d55b51630d92c5a7a485abad7673cf1f
Denisa Sandu <denisa_sandu95@yahoo.com>
2015-10-24 02:47:41 +0300

    Index + AngularJS

---
+++ js/app.js
@@ -0,0 +1,31 @@
+function myriddles($scope){
+    $scope.riddles = [
+    {
+      name: "Vârsta",
+      enunt: "în urmă cu două zile, Mihai avea 7 ani. Anul viitor, el va împlini 10 ani. Cum este posibil?",
+      solutie: "Astăzi este 1 ianuarie. Ieri, pe 31 decembrie, Mihai a făcut 8 ani. Pe 30 decembrie, el avea încă 7 ani. Anul acesta el va împlini 9 ani, iar anul următor va împlini 10.",
+      dificultate: 2,
+      active1: false,
+      active2: false
+    },
+    {
+      name: "Vârsta",
+      enunt: "în urmă cu două zile, Mihai avea 7 ani. Anul viitor, el va împlini 10 ani. Cum este posibil?",
+      solutie: "Astăzi este 1 ianuarie. Ieri, pe 31 decembrie, Mihai a făcut 8 ani. Pe 30 decembrie, el avea încă 7 ani. Anul acesta el va împlini 9 ani, iar anul următor va împlini 10.",
+      dificultate: 2,
+      active1: false,
+      active2: false
+    },
+    {
+      name: "Vârsta",
+      enunt: "în urmă cu două zile, Mihai avea 7 ani. Anul viitor, el va împlini 10 ani. Cum este posibil?",
+      solutie: "Astăzi este 1 ianuarie. Ieri, pe 31 decembrie, Mihai a făcut 8 ani. Pe 30 decembrie, el avea încă 7 ani. Anul acesta el va împlini 9 ani, iar anul următor va împlini 10.",
+      dificultate: 2,
+      active1: false,
+      active2: false
+    }
+  ];
+
+
+  
+}
